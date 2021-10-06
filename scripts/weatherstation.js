f0077926c234ffaddc6c9be94fc5a340a8163265
Molitor, Matthew E <matthew.molitor@wsu.edu>
2021-10-06 14:52:29 -0700

    changed api to national weather service

--- scripts/weatherstation.js
+++ scripts/weatherstation.js
@@ -32,24 +32,29 @@ class WeatherStation extends React.Component {
     }
 
     getCurrentObservations = async() => {
-        const response = await fetch('http://api.openweathermap.org/data/2.5/weather?lat=' +               
-          this.state.latitude + '&lon=' +
-          this.state.longitude + '&appid=98cb8d2538da248784d8e1c1f9332ea9');
+        const response1 = await fetch('https://api.weather.gov/points/'+               
+          this.state.latitude + ',' +
+          this.state.longitude);
+        //get values for lat + long based on prior call
+        const response = await fetch('https://api.weather.gov/gridpoints/' +
+        'OTX/141,45/forecast');             
+ 
         const currWeather = await response.json();
         this.setState({place: currWeather.name,
             retrieved: (new Date()).toLocaleDateString() + " at " + 
                (new Date()).toLocaleTimeString(),
-            conditions: currWeather.weather[0].main,
-            visibility: currWeather.weather.visibility,
-            visibilityUnit: "Meters",
-            temp: Math.round(currWeather.main.temp - 273.15),
-            tempUnit: "C",
-            humidity: currWeather.main.humidity,
-            visibility: currWeather.visibility,
-            wind: currWeather.wind.speed,
-            windUnit: "Meters/sec",
-            windDirection: currWeather.wind.deg,
-            windDirectionUnit: "Degrees"
+            conditions: currWeather.properties.periods[0].shortForecast,
+            // no property for visibility
+            //visibility: currWeather.properties.periods[0].visibility,
+            //visibilityUnit: "Meters",
+            temp: Math.round(currWeather.properties.periods[0].temperature),
+            tempUnit: "F",
+            //humidity: currWeather.main.humidity,
+            //visibility: currWeather.visibility,
+            wind: currWeather.properties.periods[0].windSpeed,
+            //windUnit: "mph",
+            windDirection: currWeather.properties.periods[0].windDirection,
+            //windDirectionUnit: "Cardinal Directions"
         });
     }
 
@@ -61,10 +66,9 @@ class WeatherStation extends React.Component {
                 <p>Conditions: {this.state.conditions}</p>
                 <p>Visibility: {this.state.visibility + " " + this.state.visibilityUnit}</p>
                 <p>Temp: {this.state.temp}&deg;&nbsp;{this.state.tempUnit}</p>
-                <p>Humidity: {this.state.humidity}%</p>
-                <p>Wind Speed: {this.state.wind + " " + this.state.windUnit}</p>
-                <p>Wind Direction: {this.state.windDirection + " " + 
-                    this.state.windDirectionUnit}</p>
+               
+                <p>Wind Speed: {this.state.wind }</p>
+                <p>Wind Direction: {this.state.windDirection }</p>
             <div className="custom-control custom-switch">
                 <input type="checkbox" className="custom-control-input" 
                        id={"switch-" + this.props.stationId} onClick={this.toggleUnits} />
